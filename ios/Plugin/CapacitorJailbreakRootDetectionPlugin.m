6fe71b3509ecb2e4800f88d81d88e503584656d2
Phu Hoang <hmphu.it@gmail.com>
2023-05-24 21:53:20 +0700

    Correct IOS detection definitions

--- ios/Plugin/CapacitorJailbreakRootDetectionPlugin.m
+++ ios/Plugin/CapacitorJailbreakRootDetectionPlugin.m
@@ -5,4 +5,6 @@
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(CapacitorJailbreakRootDetectionPlugin, "CapacitorJailbreakRootDetection",
            CAP_PLUGIN_METHOD(isJailbrokenOrRooted, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(isSimulator, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(isDebuggedMode, CAPPluginReturnPromise);
 )
